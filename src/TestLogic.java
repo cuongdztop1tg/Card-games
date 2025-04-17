4a7982d2b4daa7676725e12b5105e1adaa3d2306
cuongdztop1tg <caocuong090905@gmail.com>
2025-04-17 10:57:52 +0700

    Nhap

--- src/TestLogic.java
+++ src/TestLogic.java
@@ -1,6 +1,6 @@
 import games.SamLocGameLogic;
 import models.Card;
-import models.DeckOfCards;
+import models.Deck;
 import models.enums.Rank;
 import models.enums.Suit;
 
@@ -11,17 +11,19 @@ import static java.util.Arrays.sort;
 public class TestLogic {
     public static void main(String[] args) {
         SamLocGameLogic logic = new SamLocGameLogic();
-        DeckOfCards deck = new DeckOfCards();
+        Deck deck = new Deck();
 
         Card c1 = new Card(Suit.HEARTS, Rank.SIX);
         Card c2 = new Card(Suit.DIAMONDS, Rank.SIX);
         Card c3 = new Card(Suit.DIAMONDS, Rank.FIVE);
         Card c4 = new Card(Suit.SPADES, Rank.FOUR);
         Card c5 = new Card(Suit.DIAMONDS, Rank.FIVE);
-        Card c6 = new Card(Suit.HEARTS, Rank.SEVEN);
+        Card c6 = new Card(Suit.HEARTS, Rank.FOUR);
+        Card c7 = new Card(Suit.DIAMONDS, Rank.SEVEN);
+        Card c8 = new Card(Suit.SPADES, Rank.SEVEN);
 
-        List<Card> cards = new ArrayList<>(Arrays.asList(c1, c2, c3, c4, c5, c6));
+        List<Card> cards = new ArrayList<>(Arrays.asList(c1, c2, c3, c4, c5, c6, c7, c8));
 
-        System.out.println(logic.isThreeConsecutivePairs(cards));
+        System.out.println(logic.isNConsecutivePairs(cards));
     }
 }
